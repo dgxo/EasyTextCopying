b9472da53e1efb0a968c14eb1a3d1a67d31e1c9b
dgxo <104234930+dgxo@users.noreply.github.com>
2024-08-11 00:26:00 +0100

    revamp - added sections and better saving

--- index.js
+++ index.js
@@ -1,27 +1,69 @@
-const copiedNotification = document.querySelector('.copied');
-const addButton = document.querySelector('button.add');
-const scriptTemplate = document.querySelector('#scriptTemplate');
+const copiedNotification = document.querySelector('#copied');
+const newSectionButton = document.querySelector('button#new-section');
+const main = document.querySelector('main');
+const snippetTemplate = document.querySelector('#snippetTemplate');
+const sectionTemplate = document.querySelector('#sectionTemplate');
 const separator = '||SEP||';
 
 function saveData() {
-	let scripts = [];
+	let snippets = {};
 
-	document.querySelectorAll('li.script').forEach((script) => {
-		scripts.push(script.querySelector('textarea').value);
+	main.querySelectorAll('section').forEach((section) => {
+		const sectionName = section.querySelector('h1 span.section-title').textContent;
+		let sectionSnippets = [];
+
+		section.querySelectorAll('li.snippet').forEach((snippet) => {
+			const value = snippet.querySelector('textarea').value;
+			if (value == '') return;
+			sectionSnippets.push(value);
+		});
+
+		snippets[sectionName] = sectionSnippets;
 	});
 
-	localStorage.setItem('scripts', scripts.join(separator));
+	localStorage.setItem('snippets', JSON.stringify(snippets));
 }
 
-function registerScript(script, editing = false) {
-	const editButton = script.querySelector('button.edit');
-	const copyButton = script.querySelector('button.copy');
-	const doneButton = script.querySelector('button.done');
-	const deleteButton = script.querySelector('button.delete');
-	const textInput = script.querySelector('textarea');
+function createSection(name, focus = false) {
+	const section = sectionTemplate.content.querySelector('section').cloneNode(true);
+	const title = section.querySelector('h1 span.section-title');
+	const icon = section.querySelector('h1 .material-symbols-outlined');
+	const addButton = section.querySelector('button#add');
+
+	icon.addEventListener('click', () => {
+		title.focus();
+		window.getSelection().selectAllChildren(title);
+	});
+
+	title.textContent = name;
+	if (focus) title.focus();
+
+	addButton.addEventListener('click', () => {
+		createSnippet('', section, true);
+		console.log(`Creating snippet in section ${title.textContent}`);
+	});
+
+	main.appendChild(section);
+
+	return section;
+}
+
+function createSnippet(content, section, focus = false) {
+	const snippet = snippetTemplate.content.querySelector('li.snippet').cloneNode(true);
+
+	const editButton = snippet.querySelector('button#edit');
+	const copyButton = snippet.querySelector('button#copy');
+	const doneButton = snippet.querySelector('button#done');
+	const deleteButton = snippet.querySelector('button#delete');
+	const textInput = snippet.querySelector('textarea');
+
+	if (typeof content !== 'string') {
+		content = '';
+	}
+
+	textInput.value = content;
 
 	function toggleEditing() {
-		console.log('toggling editing');
 		doneButton.classList.toggle('hidden');
 		editButton.classList.toggle('hidden');
 		copyButton.classList.toggle('hidden');
@@ -29,7 +71,10 @@ function registerScript(script, editing = false) {
 	}
 
 	// default is not editing
-	if (editing) toggleEditing();
+	if (focus) {
+		toggleEditing();
+		textInput.focus();
+	}
 
 	editButton.addEventListener('click', () => {
 		toggleEditing();
@@ -49,28 +94,37 @@ function registerScript(script, editing = false) {
 	});
 
 	deleteButton.addEventListener('click', () => {
-		script.remove();
+		snippet.remove();
+		saveData();
 	});
+
+	section.querySelector('ul').append(snippet);
 }
 
-function createScript(content, editing = false) {
-	if (typeof content !== 'string') {
-		content = '';
+let snippets = localStorage.getItem('snippets');
+let scripts = localStorage.getItem('scripts');
+
+if (scripts) {
+	const section = createSection('Old Scripts', true);
+
+	for (const snippet of scripts.split(separator)) {
+		createSnippet(snippet, section, false);
 	}
 
-	let script = scriptTemplate.content.querySelector('li.script').cloneNode(true);
-	document.querySelector('main ul').append(script);
-	script.querySelector('textarea').value = content;
-	if (editing) script.querySelector('textarea').focus();
-	registerScript(script, editing);
-}
+	saveData();
+	localStorage.removeItem('scripts');
+} else if (snippets) {
+	snippets = JSON.parse(snippets);
+	for (const sectionName in snippets) {
+		const section = createSection(sectionName);
 
-const savedData = localStorage.getItem('scripts');
-if (savedData) {
-	for (const script of localStorage.getItem('scripts').split(separator)) {
-		createScript(script, false);
+		for (const snippet of snippets[sectionName]) {
+			createSnippet(snippet, section, false);
+		}
 	}
 }
-addButton.addEventListener('click', () => {
-	createScript('', true);
+
+newSectionButton.addEventListener('click', () => {
+	const section = createSection('Section', true);
+	createSnippet('', section, true);
 });
